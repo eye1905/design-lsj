95aaa3720c1af37981ce4465aa941fb6f0354ae8
eye1905 <cfcyoga@gmail.com>
2019-11-12 23:40:30 +0700

    udpate desain ahaha

--- bootstrap/dist/js/template.js
+++ bootstrap/dist/js/template.js
@@ -32,49 +32,4 @@ $(document).ready(function() {
 			$(".row-category").show();
 		}
 	});
-	
-	var $item = $('.carousel-item');
-	var $wHeight = $(window).height();
-
-	$item.height($wHeight);
-	$item.addClass('full-screen');
-
-	$('.carousel img').each(function() {
-		var $src = $(this).attr('src');
-		var $color = $(this).attr('data-color');
-		$(this).parent().css({
-			'background-image' : 'url(' + $src + ')',
-			'background-color' : $color
-		});
-		$(this).remove();
-	});
-
-	$(window).on('resize', function (){
-		$wHeight = $(window).height();
-		$item.height($wHeight);
-	});
-
-	$("#myCarousel").on("slide.bs.carousel", function(e) {
-    var $e = $(e.relatedTarget);
-    var idx = $e.index();
-    var itemsPerSlide = 3;
-    var totalItems = $(".carousel-item").length;
-
-    if (idx >= totalItems - (itemsPerSlide - 1)) {
-      var it = itemsPerSlide - (totalItems - idx);
-      for (var i = 0; i < it; i++) {
-        // append slides to end
-        if (e.direction == "left") {
-          $(".carousel-item")
-            .eq(i)
-            .appendTo(".carousel-inner");
-        } else {
-          $(".carousel-item")
-            .eq(0)
-            .appendTo($(this).find(".carousel-inner"));
-        }
-      }
-    }
-  });
-	
 });
