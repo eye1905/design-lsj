107b4f8efa152fa1e77866a5d3be3a7fab15b653
eye1905 <cfcyoga@gmail.com>
2019-11-13 10:42:54 +0700

    update lagi

--- bootstrap/dist/js/template.js
+++ bootstrap/dist/js/template.js
@@ -6,6 +6,7 @@ $(document).ready(function() {
 		$(".card-rounded").css("width", "22rem");
 		$(".card-rounded").css("margin-left", "5px");
 		$(".row-category").hide();
+		$(".back-welcome").css("background-image", "url('img/banner2.png')");
 	}
 	else {
 		$(".nav-item1").css("margin-left", "10px");
@@ -13,6 +14,7 @@ $(document).ready(function() {
 		$(".card-rounded").css("width", "17rem");
 		$(".card-rounded").css("margin-left", "20px");
 		$(".row-category").show();
+		$(".back-welcome").css("background-image", "url('img/banner.png')");
 	}
 
 	// when ready but inframe inspect
@@ -23,6 +25,7 @@ $(document).ready(function() {
 			$(".card-rounded").css("width", "22rem");
 			$(".card-rounded").css("margin-left", "5px");
 			$(".row-category").hide();
+			$(".back-welcome").css("background-image", "url('img/banner2.png')");
 		}
 		else {
 			$(".nav-item1").css("margin-left", "10px");
@@ -30,6 +33,7 @@ $(document).ready(function() {
 			$(".card-rounded").css("width", "17rem");
 			$(".card-rounded").css("margin-left", "20px");
 			$(".row-category").show();
+			$(".back-welcome").css("background-image", "url('img/banner.png')");
 		}
 	});
 });
